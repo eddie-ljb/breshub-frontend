f6c16398c4a6efbe5d6753942c2c15676bbc8ea4
Etienne Bader <baderetienne@gmail.com>
2025-03-17 10:20:15 +0100

    ref: build fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
+import ViteStaticCopy from 'vite-plugin-static-copy';
 
 export default defineConfig({
     server: {
         allowedHosts: ['.etiennebader.de']
-    }
+    },
+    build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: 'assets/[name].[ext]',
+          },
+        },
+      },
+      resolve: {
+        alias: {
+          '@': '/src',
+        },
+      },
+      plugins: [
+        ViteStaticCopy({
+          targets: [
+            {
+              src: 'node_modules/primeicons/fonts/*',
+              dest: 'fonts',
+            },
+          ],
+        }),
+      ],
 })
